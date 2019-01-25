1d2e016b7c966bc3e43a8a31a2ebe342e6882c9b
Negar Amiri <negamiri@gmail.com>
2019-01-25 06:06:23 +0000

    Routes set up for non-admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'cities#index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:new, :create, :edit]
+
+  resources :trips, except: [:index]
+
+  resources :itineraries, except: [:index]
+
+  resources :cities, only: [:show, :index]
+
+  resources :attractions, only: [:create, :edit, :update, :show] do
+    resources :reviews, only: [:create]
+  end
+
+  resources :itineraryitems, only: [:create, :edit, :update, :show, :destroy]
+
+  resources :reviews, only: [:destroy]
+
 end
