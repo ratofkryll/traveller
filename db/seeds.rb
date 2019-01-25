5d0bf34da476b329bc14c96142f4289ef2e32011
Gioanna Rheumer <ratofkryll@gmail.com>
2019-01-25 02:02:10 +0000

    created city seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,50 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
 
-require 'faker'
+require "faker"
 
-puts 'Seeding data...'
+puts "Seeding data..."
 
-# Helper functions
-def open_asset(file_name)
-  File.open(Rails.root.join('db', 'seed_assets', file_name))
-end
+# CITIES
 
-# Only run on development (local) instances not on production, etc.
-unless Rails.env.development?
-  puts "Development seeds only (for now)!"
-  exit 0
-end
+puts "Finding or creating cities..."
+
+city1 = City.find_or_create_by!({
+  name: "Vancouver",
+  country: "Canada",
+  description: Faker::Lovecraft.paragraph(3),
+  language: "English",
+  currency: "CAD",
+  transit: "https://www.translink.ca/",
+  emergency_phone: "911",
+  time_zone: "PST (UTC-8h)",
+  tipping_custom: "15-18%",
+  image: "assets/images/city_vancouver.jpg"
+})
+city2 = City.find_or_create_by!({
+  name: "Montreal",
+  country: "Canada",
+  description: Faker::Lovecraft.paragraph(3),
+  language: "French/English",
+  currency: "CAD",
+  transit: "http://www.stm.info/en",
+  emergency_phone: "911",
+  time_zone: "EST (UTC-5h)",
+  tipping_custom: "15-18%",
+  image: "assets/images/city_montreal.jpg"
+})
+city3 = City.find_or_create_by!({
+  name: "London",
+  country: "England",
+  description: Faker::Lovecraft.paragraph(3),
+  language: "English",
+  currency: "GBP",
+  transit: "http://www.londontransit.ca/",
+  emergency_phone: "999",
+  time_zone: "GMT (UTC+0h)",
+  tipping_custom: "Usually 12.75% added to your bill.",
+  image: "assets/images/city_london.jpg"
+})
