bb99718b945e2dde7816d702f3f1c7670df8f394
Gioanna Rheumer <ratofkryll@gmail.com>
2019-01-26 00:47:26 +0000

    fixed now-ui bug

---
+++ app/assets/javascripts/now-ui-kit.js
@@ -0,0 +1,17 @@
+//= require now-ui-kit/core/popper.min
+//= require now-ui-kit/core/bootstrap.min
+//= require now-ui-kit/plugins/moment.min
+// Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/
+//= require now-ui-kit/plugins/bootstrap-switch
+// Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs
+//= require now-ui-kit/plugins/bootstrap-tagsinput
+// Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select
+//= require now-ui-kit/plugins/bootstrap-selectpicker
+// Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput
+//= require now-ui-kit/plugins/jasny-bootstrap.min
+// Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/
+//= require now-ui-kit/plugins/nouislider.min
+// Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker
+//= require now-ui-kit/plugins/bootstrap-datetimepicker.min
+// Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc
+//= require now-ui-kit/now-ui-kit
