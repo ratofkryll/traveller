9af8f6a53ef8a6931fa5100250a62490731e11be
Negar Amiri <negamiri@gmail.com>
2019-01-26 03:59:04 +0000

    Add invite email logic

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,17 +1,15 @@
 class InvitesController < ApplicationController
-  # before_action :check_recipient
-
-  # def check_recipient
-    # recipient = User.find_by_email(email)
-    # if recipient
-    #   self.recipient = recipient.id
-    # end
-  # end
 
   def create
     @invite = Invite.new(invite_params)
     if @invite.save
-       InviteMailer.register_invite(@invite, new_user_registration_path).deliver
+      if @invite.recipient != nil
+        InviteMailer.existing_user_invite(@invite).deliver
+        trip = User_trip.find_by_id(@invite.user_trip_id)
+        User_trip.create({user_id: @invite.recipient, trip_id: trip.trip_id, role: 'contributor'})
+      else 
+        InviteMailer.register_invite(@invite, new_user_registration_path(invite_token: @invite.token)).deliver
+      end
     else
        # oh no, creating an new invitation failed
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def new
     @token = params[:invite_token]
   end
+
+  def create
+    @newUser = User.new(user_params)
+    @newUser.save
+    @token = params[:invite_token]
+    if @token != nil
+       trip =  Invite.find_by_token(@token).user_trip
+       @newUser.user_trips.create({trip_id: trip.id, role: 'contributor'})
+    else
+      # do normal registration things #
+    end
+  end
+
+
 end
