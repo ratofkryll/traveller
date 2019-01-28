2c1ba9b46c65fd65cb0b670c9da0b2906bd7e90c
Negar Amiri <negamiri@gmail.com>
2019-01-28 02:11:55 +0000

    Add attractions to cities show page

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,9 +1,9 @@
 class CitiesController < ApplicationController
   def show
     @city = City.find params[:id]
-    @attraction = @city.attractions
+    @attractions = @city.attractions
   end
-
+ 
   def index
     @cities = City.all.order(name: :desc)
   end
