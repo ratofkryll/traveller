1bda7db304078178245dfe5423c1a90a2df80045
Gioanna Rheumer <ratofkryll@gmail.com>
2019-02-05 12:25:33 -0800

    set up action cable for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,9 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.web_socket_server_url = "wss://traveller-lhl.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://traveller-lhl.herokuapp.com', 'http://traveller-lhl.herokuapp.com']
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
